217537e22338a10a59353fc783a5a3ab5e5e752a
lsy <2547200173@qq.com>
2020-08-04 23:01:05 +0800

    day03,day04作业

---
+++ jiyun-service-consumer/src/main/java/cn/jiyun/service/jiyunserviceconsumer/service/UserService.java
@@ -0,0 +1,23 @@
+package cn.jiyun.service.jiyunserviceconsumer.service;
+
+import cn.jiyun.service.jiyunserviceconsumer.dao.UserDAO;
+import cn.jiyun.service.jiyunserviceconsumer.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserDAO userDAO;
+
+    public List<User> queryUserById(List<Long> ids){
+        List<User> users = new ArrayList<>();
+        ids.forEach(id->{
+            users.add(this.userDAO.queryUserById(id));
+        });
+        return users;
+    }
+}
